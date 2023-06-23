5a1b0a94103b008c5728796864c700f88a32a268
Bogdan Vulturar <84024748+bogdanvulturar17@users.noreply.github.com>
2023-06-23 19:55:46 +0300

    api_madlib.py

--- api_madlib.py
+++ api_madlib.py
@@ -24,7 +24,7 @@ def get_data():
     mobile_price_prediction_api = parameters['mobile_price_prediction_api']
     prediction_results_mobile_price_prediction = parameters['prediction_results_mobile_price_prediction']
 
-    #crating a copy of the initial table, running the prediction algorithm, showing the scoring results
+    #creating a copy of the initial table, running the prediction algorithm, showing the scoring results
     insert = f"CREATE TABLE IF NOT EXISTS {mobile_price_prediction_api} AS SELECT * FROM {mobile_price_prediction}"
     delete_prediction_results = f"DROP TABLE IF EXISTS {prediction_results_mobile_price_prediction}"
     prediction = f"SELECT forest_predict('mobile_price_prediction_split_train_out', '{mobile_price_prediction_api}', '{prediction_results_mobile_price_prediction}','response');"
